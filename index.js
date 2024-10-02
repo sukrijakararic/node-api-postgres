4c1eb70660fae4a6417734d28dadf2e30124dd79
sukrijakararic <sukrijakararic1@gmail.com>
2024-10-02 21:52:16 +0200

    implemented local strategy for user log in

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const app = express()
 const db = require('./queries')
 const port = 3000
 const cors = require('cors')
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
 
 app.use(cors())
 
@@ -14,16 +16,41 @@ app.use(
   })
 )
 
+app.use(passport.initialize());
+app.use(new LocalStrategy((username, password, done) => {
+  function checkUser(error, user) {
+    if (error) {
+      return done(error);
+    }
+    if (!user) {
+      return done(null, false, { message: 'Incorrect username.' });
+    }
+    if (!user.password || user.password !== password) {
+      return done(null, false, { message: 'Incorrect password.' });
+    }
+    return done(null, user);
+  }
+  db.getUserByUsername(username, checkUser);
+}))
+
 app.get('/', (request, response) => {
   response.json({ info: 'Node.js, Express, and Postgres API' })
 })
 
 app.get('/users', db.getUsers)
 app.get('/users/:id', db.getUserById)
-app.post('/users', db.createUser)
-app.put('/users/:id', db.updateUser)
-app.delete('/users/:id', db.deleteUser)
+app.post('/users', passport.authenticate('local', {failureRedirect: '/'}), db.createUser)
+app.put('/users/:id', passport.authenticate('local', {failureRedirect: '/'}), db.updateUser)
+app.delete('/users/:id', passport.authenticate('local', {failureRedirect: '/'}), db.deleteUser)
 
+app.use((req, res, next ) => {
+  const error = new Error('Something went wrong')
+  next(error)
+})
+app.use((error, request, response, next) => {
+  console.error('Error: ', error.message)
+  res.status(500).send('Internal Server Error');
+})
 app.listen(port, () => {
   console.log(`App running on port ${port}.`)
 })
